5b31b7f850d1bc5878110bca03819a3feac801ab
Alessio Lodato <swly7j4e@students.codeinstitute.net>
2025-03-17 13:50:39 +0000

    added likes and comments counters

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Count
 from rest_framework import generics, permissions
 from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Post
@@ -9,9 +10,22 @@ class PostList(generics.ListCreateAPIView):
     List posts or create a post if logged in
     The perform_create method associates the post with the logged in user.
     """
+
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    queryset = Post.objects.all()
+    queryset = Post.objects.annotate(
+        comments_count=Count('comment', distinct=True)
+        likes_count=Count('likes', distinct=True)
+    ).order_by('-created_at')
+    filter_backends = [
+        filters.OrderingFilter
+    ]
+    ordering_fields = [
+        'likes_count',
+        'comments_count',
+        'likes__created_at',
+    ]
+
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
@@ -23,4 +37,7 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     serializer_class = PostSerializer
     permission_classes = [IsOwnerOrReadOnly]
-    queryset = Post.objects.all()
+    queryset = Post.objects.annotate(
+        comments_count=Count('comment', distinct=True)
+        likes_count=Count('likes', distinct=True)
+    ).order_by('-created_at')
