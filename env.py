bc14e1cab9c664d3a0b3de29940c9f4f57d006c1
Alessio Lodato <swly7j4e@students.codeinstitute.net>
2025-03-20 10:18:13 +0000

    update env.py file

--- env.py
+++ env.py
@@ -1,5 +1,5 @@
 import os 
 os.environ['CLOUDINARY_URL'] = 'cloudinary://787118913997848:jeLtvcQRKlcbFvU9hpkfUTA1bMU@da5fyv6lx'
-# os.environ['DEV'] = '1'
+os.environ['DEV'] = '1'
 os.environ['DATABASE_URL'] = "postgresql://neondb_owner:g7ExmvCd6Arf@ep-divine-rice-a27ex5yy.eu-central-1.aws.neon.tech/help_track_frost_638291"
 os.environ.setdefault("SECRET_KEY", "alex1ett09528!")
